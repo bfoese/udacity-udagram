92bb3d64e6ac53232f09f5a58e74c9705e3c54c8
Britt Foese <britt.foese@gmail.com>
2021-04-17 18:02:52 +0200

    feat(filter image): request implementation

--- src/util/util.ts
+++ src/util/util.ts
@@ -8,17 +8,22 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+
+        try {
+            const photo = await Jimp.read(inputURL);
+            const outpath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+            return await photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname + outpath, (img) => {
+                    resolve(__dirname + outpath);
+                });
+        } catch (error) {
+            reject(error);
+        }
     });
 }
 
@@ -27,8 +32,22 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
+export async function deleteLocalFiles(files: Array<string>) {
+    for (let file of files) {
         fs.unlinkSync(file);
     }
+}
+
+/**
+ * Syntax validation of an image URL.
+ * @param url - URL to validate
+ * @returns - `true` for an non-empty URL containing a HTTP protocol and a supported image format (jpeg, jpg, gif, png). Otherwise `false`
+ */
+export function isValidImageUrl(url: string | undefined): boolean {
+    if (!url || url.length === 0) {
+        return false
+    }
+    const protocolPattern = /^https?:\/\/.*\.(jpeg|gif|png|jpg)$/;
+    const match = url.match(protocolPattern);
+    return match && match.length > 0;
 }
